500bd8885e5b50c05e6ccf692b87f78b280b5aaa
Marcel Lilienthal <134974+mlilien@users.noreply.github.com>
2021-07-20 09:21:07 +0200

    enable parallel execution of tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,16 +1,23 @@
+use std::fs::{create_dir,remove_dir_all};
 use fs_extra::dir::copy;
 use fs_extra::dir::CopyOptions;
 
-pub fn setup() {
-    copy("testfiles", "tests", &CopyOptions{
+pub fn setup(prefix: &str) {
+    create_dir(prefix).unwrap();
+    copy("testfiles", prefix, &CopyOptions{
         overwrite: true,
         ..Default::default()}).unwrap_or_else(|err| {
             // ignore all errors if dir cannot be deleted
             println!("Problem copy: {}", err);
             1
         });
+
 }
 
-pub fn cleanup() {
+pub fn cleanup(prefix: &str) {
     // place your cleanup code here
+    remove_dir_all(prefix).unwrap_or_else(|err| {
+        // ignore all errors if dir cannot be deleted
+        println!("Problem remove_dir_all: {}", err);
+    });
 }
