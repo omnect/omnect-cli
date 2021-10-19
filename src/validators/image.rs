b8bea7cb2321c13eefa52d48569ce50572e51fc9
Roland Erk <roland.erk@conplement.de>
2021-10-19 14:30:09 +0200

    Refactor handling of compression algorithms, avoiding repetitive code.

--- src/validators/image.rs
+++ src/validators/image.rs
@@ -4,65 +4,57 @@ use std::path::PathBuf;
 use std::fs::remove_file;
 use tokio::fs::File;
 
-#[tokio::main]
-async fn decompress_xz(image_file_name: &PathBuf) -> Result<PathBuf, tokio::io::Error> {
-    let mut new_image_file = image_file_name.to_path_buf();
-    new_image_file.set_extension("unxz.tmp");
-    let destination = File::create(new_image_file.clone()).await?;
-    let mut source = File::open(image_file_name).await?;
-    let mut decompressor = async_compression::tokio::write::XzDecoder::new(destination);
-    tokio::io::copy(&mut source, &mut decompressor).await?;
-    return Ok(new_image_file);
+
+trait CompressionGenerator {
+    fn compressor<'a>(&self, destination: &'a mut(dyn tokio::io::AsyncWrite + Unpin))-> Box<dyn tokio::io::AsyncWrite + Unpin + 'a>;
+    fn decompressor<'a>(&self, destination: &'a mut(dyn tokio::io::AsyncWrite + Unpin)) ->Box<dyn tokio::io::AsyncWrite + Unpin + 'a>;
 }
 
-#[tokio::main]
-async fn compress_xz(uncompressed_file_name: &PathBuf, compressed_file_name: &PathBuf) -> Result<(), tokio::io::Error> {
-    let destination = File::create(compressed_file_name.clone()).await?;
-    let mut source = File::open(uncompressed_file_name).await?;
-    let mut decompressor = async_compression::tokio::write::XzEncoder::new(destination);
-    tokio::io::copy(&mut source, &mut decompressor).await?;
-    return Ok(());
+struct XzGenerator;
+impl  CompressionGenerator for XzGenerator
+{
+    fn compressor<'a>(&self, destination: &'a mut(dyn tokio::io::AsyncWrite + Unpin)) -> Box<dyn tokio::io::AsyncWrite+ Unpin + 'a> {
+        Box::new(async_compression::tokio::write::XzEncoder::new(destination))
+    }
+    fn decompressor<'a>(&self, destination: &'a mut(dyn tokio::io::AsyncWrite + Unpin)) -> Box<dyn tokio::io::AsyncWrite+ Unpin + 'a> {
+        Box::new(async_compression::tokio::write::XzDecoder::new(destination))
+    }
 }
 
-#[tokio::main]
-async fn decompress_bzip2(image_file_name: &PathBuf) -> Result<PathBuf, tokio::io::Error> {
-    let mut new_image_file = image_file_name.to_path_buf();
-    new_image_file.set_extension("bzip.tmp");
-    let destination = File::create(new_image_file.clone()).await?;
-    let mut source = File::open(image_file_name).await?;
-    let mut decompressor = async_compression::tokio::write::BzDecoder::new(destination);
-    tokio::io::copy(&mut source, &mut decompressor).await?;
-    return Ok(new_image_file);
+struct BzGenerator;
+impl  CompressionGenerator for BzGenerator
+{
+    fn compressor<'a>(&self, destination: &'a mut(dyn tokio::io::AsyncWrite + Unpin)) ->  Box<dyn tokio::io::AsyncWrite+ Unpin + 'a> {
+        Box::new(async_compression::tokio::write::BzEncoder::new(destination))
+    }
+    fn decompressor<'a>(&self, destination: &'a mut(dyn tokio::io::AsyncWrite + Unpin)) ->  Box<dyn tokio::io::AsyncWrite+ Unpin + 'a> {
+        Box::new(async_compression::tokio::write::BzDecoder::new(destination))
+    }
 }
 
-#[tokio::main]
-async fn compress_bzip(uncompressed_file_name: &PathBuf, compressed_file_name: &PathBuf) -> Result<(), tokio::io::Error> {
-    let destination = File::create(compressed_file_name.clone()).await?;
-    let mut source = File::open(uncompressed_file_name).await?;
-    let mut decompressor = async_compression::tokio::write::BzEncoder::new(destination);
-    tokio::io::copy(&mut source, &mut decompressor).await?;
-    return Ok(());
+struct GzGenerator;
+impl  CompressionGenerator for GzGenerator
+{
+    fn compressor<'a>(&self, destination: &'a mut(dyn tokio::io::AsyncWrite + Unpin)) ->  Box<dyn tokio::io::AsyncWrite+ Unpin + 'a> {
+        Box::new(async_compression::tokio::write::GzipEncoder::new(destination))
+    }
+    fn decompressor<'a>(&self, destination: &'a mut(dyn tokio::io::AsyncWrite + Unpin)) ->  Box<dyn tokio::io::AsyncWrite+ Unpin + 'a> {
+        Box::new(async_compression::tokio::write::GzipDecoder::new(destination))
+    }
 }
 
-#[tokio::main]
-async fn decompress_gzip(image_file_name: &PathBuf) -> Result<PathBuf, tokio::io::Error> {
-    let mut new_image_file = image_file_name.to_path_buf();
-    new_image_file.set_extension("gz.tmp");
-    let destination = File::create(new_image_file.clone()).await?;
-    let mut source = File::open(image_file_name).await?;
-    let mut decompressor = async_compression::tokio::write::GzipDecoder::new(destination);
-    tokio::io::copy(&mut source, &mut decompressor).await?;
-    return Ok(new_image_file);
+
+struct CompressionAlternative {
+    marker: &'static str,
+    extension: &'static str,
+    generator: &'static dyn CompressionGenerator,
 }
 
-#[tokio::main]
-async fn compress_gzip(uncompressed_file_name: &PathBuf, compressed_file_name: &PathBuf) -> Result<(), tokio::io::Error> {
-    let destination = File::create(compressed_file_name.clone()).await?;
-    let mut source = File::open(uncompressed_file_name).await?;
-    let mut decompressor = async_compression::tokio::write::GzipEncoder::new(destination);
-    tokio::io::copy(&mut source, &mut decompressor).await?;
-    return Ok(());
-}
+const COMPRESSION_TABLE: [CompressionAlternative; 3] = [
+    CompressionAlternative { marker: "XZ compressed data", extension: "unxz.tmp", generator: &XzGenerator {} },
+    CompressionAlternative { marker: "bzip2 compressed data", extension: "unbzip2.tmp", generator: &BzGenerator {} },
+    CompressionAlternative { marker: "gzip compressed data", extension: "ungzip.tmp", generator: &GzGenerator {} },
+];
 
 pub fn validate_image(
     image_file_name: &PathBuf,
@@ -88,108 +80,70 @@ pub fn validate_image(
         _ => {}
     }
     let magic = detector.file(&image_file_name)?;
-    println!("magic= {}", magic);
-    if magic.find("XZ compressed data") != None {
-        println!("xz compressed file found, decompressing...");
-        let new_image_file = decompress_xz(image_file_name)?;
-        println!("Decompressed to {}", new_image_file.to_string_lossy());
-        return Ok(new_image_file);
-    } else if magic.find("bzip2 compressed data") != None {
-        println!("bzip2 compressed file found, decompressing...");
-        let new_image_file = decompress_bzip2(image_file_name)?;
-        println!("Decompressed to {}", new_image_file.to_string_lossy());
-        return Ok(new_image_file);
-    } else if magic.find("gzip compressed data") != None {
-        println!("gzip compressed file found, decompressing...");
-        let new_image_file = decompress_gzip(image_file_name)?;
-        println!("Decompressed to {}", new_image_file.to_string_lossy());
-        return Ok(new_image_file);
+    for elem in COMPRESSION_TABLE {
+        if magic.find(elem.marker) != None {
+            println!("Compressed file found, decompressing...");
+            let new_image_file = decompress(image_file_name, elem.extension, elem.generator)?;
+            println!("Decompressed to {}", new_image_file.to_string_lossy());
+            return Ok(new_image_file);
+        }
     }
     Ok(image_file_name.to_path_buf())
+}
+
+#[tokio::main]
+async fn decompress(image_file_name: &PathBuf, extension: &'static str, generator: &'static dyn CompressionGenerator ) -> Result<PathBuf, tokio::io::Error> {
+    let mut new_image_file = image_file_name.to_path_buf();
+    new_image_file.set_extension(extension);
+    let mut destination = File::create(new_image_file.clone()).await?;
+    let mut source = File::open(image_file_name).await?;
+    let mut decompressor = generator.decompressor(&mut destination);
+    tokio::io::copy(&mut source, &mut decompressor).await?;
+    return Ok(new_image_file);
+}
+
+#[tokio::main]
+async fn compress(uncompressed_file_name: &PathBuf, compressed_file_name: &PathBuf, generator: &'static dyn CompressionGenerator) -> Result<(), tokio::io::Error> {
+    let mut destination = File::create(compressed_file_name.clone()).await?;
+    let mut source = File::open(uncompressed_file_name).await?;
+    let mut decompressor = generator.compressor(&mut destination);
+    tokio::io::copy(&mut source, &mut decompressor).await?;
+    return Ok(());
 }
 
 pub fn postprocess_image(
     original_file_name: &PathBuf,
     unpacked_file_name: &PathBuf,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    if original_file_name != unpacked_file_name {
-        match unpacked_file_name.to_string_lossy().find("unxz.tmp") {
-            Some(_) => {
-                println!("xz compressing {} to {} ...", unpacked_file_name.to_string_lossy(), original_file_name.to_string_lossy());
-                match compress_xz(unpacked_file_name, original_file_name) {
-                    Ok(_e) => { 
-                        match remove_file(unpacked_file_name) {
-                            Err(e) =>{
-                                return Err(Box::new(Error::new(
-                                    ErrorKind::Other,
-                                    format!("Deleting temporary file failed with error {}", e.to_string()),
-                                )));
-                            }
-                            Ok(_) => { return Ok(()); }
-                        }
-                    }
-                    Err(e) =>{
-                        return Err(Box::new(Error::new(
-                            ErrorKind::Other,
-                            format!("Recompressing failed with error {}", e.to_string()),
-                        )));
-                    }
-                }
-            }
-            None => {}
-        }
-        match unpacked_file_name.to_string_lossy().find("bzip.tmp") {
-            Some(_) => {
-                println!("bzip2 compressing {} to {} ...", unpacked_file_name.to_string_lossy(), original_file_name.to_string_lossy());
-                match compress_bzip(unpacked_file_name, original_file_name) {
-                    Ok(_e) => {
-                        match remove_file(unpacked_file_name) {
-                            Err(e) =>{
-                                return Err(Box::new(Error::new(
-                                    ErrorKind::Other,
-                                    format!("Deleting temporary file failed with error {}", e.to_string()),
-                                )));
-                            }
-                            Ok(_) => { return Ok(()); }
-                        }
-                    }
-                    Err(e) =>{
-                        return Err(Box::new(Error::new(
-                            ErrorKind::Other,
-                            format!("Recompressing failed with error {}", e.to_string()),
-                        )));
-                    }
-                }
-            }
-            None => {}
-        }
-        match unpacked_file_name.to_string_lossy().find("gz.tmp") {
-            Some(_) => {
-                println!("gzip compressing {} to {} ...", unpacked_file_name.to_string_lossy(), original_file_name.to_string_lossy());
-                match compress_gzip(unpacked_file_name, original_file_name) {
-                    Ok(_e) => {
-                        match remove_file(unpacked_file_name) {
-                            Err(e) =>{
-                                return Err(Box::new(Error::new(
-                                    ErrorKind::Other,
-                                    format!("Deleting temporary file failed with error {}", e.to_string()),
-                                )));
-                            }
-                            Ok(_) => { return Ok(()); }
-                        }
-                    }
-                    Err(e) =>{
-                        return Err(Box::new(Error::new(
-                            ErrorKind::Other,
-                            format!("Recompressing failed with error {}", e.to_string()),
-                        )));
-                    }
-                }
-            }
-            None => {}
-        }
-
+    if original_file_name == unpacked_file_name {
+        return Ok(());
     }
+    for elem in COMPRESSION_TABLE {
+        if unpacked_file_name.to_string_lossy().find(elem.extension) != None {
+            println!("Uncompressed file found, compressing...");
+            match compress(original_file_name, unpacked_file_name, elem.generator)
+            {
+                Ok(_e) => {
+                    println!("Compressed to {}", original_file_name.to_string_lossy());
+                    match remove_file(unpacked_file_name) {
+                        Err(e) =>{
+                            return Err(Box::new(Error::new(
+                                ErrorKind::Other,
+                                format!("Deleting temporary file failed with error {}", e.to_string()),
+                            )));
+                        }
+                        Ok(_) => { return Ok(()); }
+                    }
+                }
+                Err(e) =>{
+                    return Err(Box::new(Error::new(
+                        ErrorKind::Other,
+                        format!("Recompressing failed with error {}", e.to_string()),
+                    )));
+                }
+            }
+        }
+    };
     Ok(())
 }
 
