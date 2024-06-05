4d946d03ffc2eebe0c8fc3c3d1c7d4f96be54096
Tobias Langer <tobias.langer@conplement.de>
2024-06-05 15:16:09 +0200

    test: add integration test for docker inject feature
    We therefore also restructure the docker module with an inner module. This way
    we can encapsulate the used implementation depending on the `mock` feature flag.

--- src/docker.rs
+++ src/docker.rs
@@ -1,8 +1,6 @@
 use anyhow::{Context, Result};
-use std::fs::File;
-use std::os::fd::AsFd;
+use std::io::Write;
 use std::path::PathBuf;
-use std::process::{Command, Stdio};
 
 use crate::image::Architecture;
 
@@ -14,66 +12,94 @@ impl From<Architecture> for &str {
     }
 }
 
-const COMPRESSION_LEVEL: u32 = 9;
+#[cfg(not(feature = "mock"))]
+mod inner {
+    use super::*;
 
-fn compress_image(source: File, destination: File) -> Result<File> {
-    let mut source = std::io::BufReader::new(source);
+    use std::fs::File;
+    use std::os::fd::AsFd;
+    use std::process::{Command, Stdio};
 
-    let destination = std::io::BufWriter::new(destination);
+    const COMPRESSION_LEVEL: u32 = 9;
 
-    let stream_builder = xz2::stream::MtStreamBuilder::new()
-        .threads(num_cpus::get() as u32)
-        .preset(COMPRESSION_LEVEL)
-        .encoder()
-        .context("pull_docker_image: could not create compression stream")?;
+    fn compress_image(source: File, destination: File) -> Result<File> {
+        let mut source = std::io::BufReader::new(source);
 
-    let mut stream = xz2::write::XzEncoder::new_stream(destination, stream_builder);
+        let destination = std::io::BufWriter::new(destination);
 
-    std::io::copy(&mut source, &mut stream)
-        .context("compress_docker_image: could not compress docker file")?;
+        let stream_builder = xz2::stream::MtStreamBuilder::new()
+            .threads(num_cpus::get() as u32)
+            .preset(COMPRESSION_LEVEL)
+            .encoder()
+            .context("pull_docker_image: could not create compression stream")?;
 
-    let writer = stream.finish()?;
-    Ok(writer.into_inner()?)
-}
+        let mut stream = xz2::write::XzEncoder::new_stream(destination, stream_builder);
 
-pub fn pull_image(name: impl AsRef<str>, arch: Architecture) -> Result<PathBuf> {
-    let cmd_out = Command::new("docker")
-        .args(["pull"])
-        .args(["--platform", arch.into()])
-        .arg(name.as_ref())
-        .output()
-        .context("pull_docker_image: could not run \"docker pull\" command")?;
+        std::io::copy(&mut source, &mut stream)
+            .context("compress_docker_image: could not compress docker file")?;
 
-    if !cmd_out.status.success() {
-        let cmd_out = std::str::from_utf8(&cmd_out.stderr).unwrap();
-        anyhow::bail!("Could not pull docker image: {cmd_out}");
+        let writer = stream.finish()?;
+        Ok(writer.into_inner()?)
     }
 
-    let mut child = Command::new("docker")
-        .args(["save"])
-        .arg(name.as_ref())
-        .stdout(Stdio::piped())
-        .spawn()
-        .context("pull_docker_image: could not run \"docker save\" command")?;
+    pub fn pull_image(name: impl AsRef<str>, arch: Architecture) -> Result<PathBuf> {
+        let cmd_out = Command::new("docker")
+            .args(["pull"])
+            .args(["--platform", arch.into()])
+            .arg(name.as_ref())
+            .output()
+            .context("pull_docker_image: could not run \"docker pull\" command")?;
 
-    let stdout = child.stdout.take().unwrap();
-    let image_file = File::from(stdout.as_fd().try_clone_to_owned()?);
+        if !cmd_out.status.success() {
+            let cmd_out = std::str::from_utf8(&cmd_out.stderr).unwrap();
+            anyhow::bail!("Could not pull docker image: {cmd_out}");
+        }
 
-    let out_path = std::path::PathBuf::from(format!("{}.tar.gz", name.as_ref()));
-    let out_file = std::fs::File::options()
-        .create_new(true)
-        .write(true)
-        .open(out_path.clone())
-        .context("pull_docker_image: could not create output file")?;
+        let mut child = Command::new("docker")
+            .args(["save"])
+            .arg(name.as_ref())
+            .stdout(Stdio::piped())
+            .spawn()
+            .context("pull_docker_image: could not run \"docker save\" command")?;
 
-    compress_image(image_file, out_file)?;
+        let stdout = child.stdout.take().unwrap();
+        let image_file = File::from(stdout.as_fd().try_clone_to_owned()?);
 
-    let error_code = child.wait()?;
+        let out_path = std::path::PathBuf::from(format!("{}.tar.gz", name.as_ref()));
+        let out_file = std::fs::File::options()
+            .create_new(true)
+            .write(true)
+            .open(out_path.clone())
+            .context("pull_docker_image: could not create output file")?;
 
-    if !error_code.success() {
-        let cmd_out = std::str::from_utf8(&cmd_out.stderr).unwrap();
-        anyhow::bail!("Could not save docker image: {cmd_out}");
+        compress_image(image_file, out_file)?;
+
+        let error_code = child.wait()?;
+
+        if !error_code.success() {
+            let cmd_out = std::str::from_utf8(&cmd_out.stderr).unwrap();
+            anyhow::bail!("Could not save docker image: {cmd_out}");
+        }
+
+        Ok(out_path)
     }
-
-    Ok(out_path)
 }
+
+#[cfg(feature = "mock")]
+mod inner {
+    use super::*;
+
+    pub fn pull_image(name: impl AsRef<str>, _arch: Architecture) -> Result<PathBuf> {
+        let out_path = std::path::PathBuf::from(format!("{}.tar.gz", name.as_ref()));
+        let mut out_file = std::fs::File::options()
+            .create_new(true)
+            .write(true)
+            .open(out_path.clone())
+            .context("pull_docker_image: could not create output file")?;
+
+        out_file.write_all(b"some test data")?;
+        Ok(out_path)
+    }
+}
+
+pub use inner::pull_image;
