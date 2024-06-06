ea334f822aed9147cdfe52fb455ceff44c303fd3
Tobias Langer <tobias.langer@conplement.de>
2024-06-06 15:28:37 +0200

    refactor: use compress mechanism for docker image compression

--- src/docker.rs
+++ src/docker.rs
@@ -17,31 +17,12 @@ impl From<Architecture> for &str {
 mod inner {
     use super::*;
 
+    use crate::file::compression::Compression;
     use std::fs::File;
     use std::os::fd::AsFd;
     use std::process::{Command, Stdio};
 
     const COMPRESSION_LEVEL: u32 = 9;
-
-    fn compress_image(source: File, destination: File) -> Result<File> {
-        let mut source = std::io::BufReader::new(source);
-
-        let destination = std::io::BufWriter::new(destination);
-
-        let stream_builder = xz2::stream::MtStreamBuilder::new()
-            .threads(num_cpus::get() as u32)
-            .preset(COMPRESSION_LEVEL)
-            .encoder()
-            .context("pull_docker_image: could not create compression stream")?;
-
-        let mut stream = xz2::write::XzEncoder::new_stream(destination, stream_builder);
-
-        std::io::copy(&mut source, &mut stream)
-            .context("compress_docker_image: could not compress docker file")?;
-
-        let writer = stream.finish()?;
-        Ok(writer.into_inner()?)
-    }
 
     pub fn pull_image(name: impl AsRef<str>, arch: Architecture) -> Result<PathBuf> {
         let cmd_out = Command::new("docker")
@@ -64,16 +45,19 @@ mod inner {
             .context("pull_docker_image: could not run \"docker save\" command")?;
 
         let stdout = child.stdout.take().unwrap();
-        let image_file = File::from(stdout.as_fd().try_clone_to_owned()?);
+        let mut image_file = File::from(stdout.as_fd().try_clone_to_owned()?);
 
         let out_path = std::path::PathBuf::from(format!("{}.tar.xz", name.as_ref()));
-        let out_file = std::fs::File::options()
+        let mut out_file = std::fs::File::options()
             .create_new(true)
             .write(true)
             .open(out_path.clone())
             .context("pull_docker_image: could not create output file")?;
 
-        compress_image(image_file, out_file)?;
+        let xz = Compression::xz {
+            compression_level: COMPRESSION_LEVEL,
+        };
+        xz.compress(&mut image_file, &mut out_file)?;
 
         let error_code = child.wait()?;
 
