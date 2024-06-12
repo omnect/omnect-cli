deb7db289b968bb518a8521d5278db9a545b4be9
Tobias Langer <tobias.langer@conplement.de>
2024-06-12 10:48:14 +0200

    test(docker): disable integration tests (#122)

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -991,8 +991,13 @@ Please remove config file first."#
     assert_eq!(config_content_before, &config_content_after);
 }
 
+// currently disabled as we have no way to test this in our pipeline were we
+// don't have docker installed
+#[ignore]
 #[test]
 fn check_docker_inject_image_success() {
+    let image = std::env::var("TEST_DOCKER_IMAGE").unwrap_or("busybox".to_string());
+
     let tr = Testrunner::new(function_name!().split("::").last().unwrap());
 
     let image_path = tr.to_pathbuf("testfiles/image.wic");
@@ -1001,7 +1006,7 @@ fn check_docker_inject_image_success() {
     let assert = docker_inject_image_config
         .arg("docker")
         .arg("inject")
-        .args(["--docker-image", "some-image"])
+        .args(["--docker-image", &image])
         .args(["--image", &image_path.to_string_lossy()])
         .args(["--partition", "factory"])
         .args(["--dest", "/some/test/dir"])
